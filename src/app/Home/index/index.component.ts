82f5d21fcf0f1390b80e76f197dc1ac46e2a5db9
praneeth chedella <praneeth5252@gmail.com>
2021-11-08 21:13:06 +0100

    changes related to reading from database

--- src/app/Home/index/index.component.ts
+++ src/app/Home/index/index.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { Component, OnInit, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgImageSliderComponent } from 'ng-image-slider';
-import {CommonModule} from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from "@angular/common/http";
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router'; import {
+  HttpClient,
+  HttpClientJsonpModule,
+  HttpParams,
+  HttpHeaders,
+} from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 
 
 export interface videoInfo_index {
-  videoId:number,
-  src:string,
-  title:string,
-  matchId:number,
-  matchType:string,
-  showOnTop:boolean,
-  img:string,
-  index:number
+  videoId: number,
+  src: string,
+  title: string,
+  matchId: number,
+  matchType: string,
+  showOnTop: boolean,
+  img: string,
+  index: number,
+  url:string,
+  showurl:boolean
+
 
 }
 
@@ -24,98 +31,175 @@ export interface videoInfo_index {
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-   
-   match=0;
-   videoId=0;
-   isInit:boolean=true;
-   
-  topVideo:videoInfo_index={matchId:0,matchType:"",videoId:0,src:"",showOnTop:false,title:"",img:"",index:0};
-  innings_1:Array<videoInfo_index>=[];
-  innings_2:Array<videoInfo_index>=[];
-  liveVideo:videoInfo_index={matchId:0,matchType:"",videoId:0,src:"",showOnTop:false,title:"",img:"",index:0};
-  
-  constructor(private route: ActivatedRoute,private httpClient: HttpClient,private _sanitizer:DomSanitizer) { }
 
-  ngOnInit(): void {
+  match = 0;
+  videoId = 0;
+  isInit: boolean = true;
+  show_innings1: boolean = false;
+  show_innings2: boolean = false;
+
+  topVideo: videoInfo_index = { matchId: 0, matchType: "", videoId: 0, src: "", showOnTop: false, title: "", img: "", index: 0 ,url:"",showurl:false};
+  innings_1: Array<videoInfo_index> = [];
+  innings_2: Array<videoInfo_index> = [];
+
+  inning_1_info: Array<videoInfo_index> = [];
+  inning_2_info: Array<videoInfo_index> = [];
+  liveVideo: videoInfo_index = { matchId: 0, matchType: "", videoId: 0, src: "", showOnTop: false, title: "", img: "", index: 0,url:"",showurl:false };
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient, private _sanitizer: DomSanitizer) { }
+
+  async ngOnInit() {
     console.log("Came here to check data")
     this.route.queryParams.subscribe(params => {
-      this.match = params['matchId']!=null?params["matchId"]:0;
-      this.videoId=params['videoId'];
+      this.match = params['matchId'] != null ? params["matchId"] : 0;
+      this.videoId = params['videoId'];
 
     });
-    if(this.isInit){
-      this.loadVideos(this.match,this.videoId);
-      this.isInit=false;
+    if (this.isInit) {
+      this.loadVideos(this.match, this.videoId);
+      this.isInit = false;
     }
-    else{
-      this.swapVideos(this.match,this.videoId);
+    else {
+      this.swapVideos(this.match, this.videoId);
     }
+
+
+  }
+
+  async loadVideos(selectedMatchId: number, selectedVideoId: number) {
+    console.log(this.match + "matchId true")
+
+    var corsHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+    const vidData = await this.httpClient
+      .get(
+        'http://localhost:3000/api/match/getmatch/60cd39a12523c92d20603802',
+        { headers: corsHeaders }
+      )
+      .toPromise();
+    console.log(vidData + "this info");
+    var arr_selected ;
     
-    
+    for(var i=0;i<Object.keys(vidData).length;i++){
+      if(vidData[i]._id==this.match){
+       arr_selected = vidData[i];
+   
+      }
+    }
+    console.log("found the array")
+    console.log(arr_selected)
+
+
+    if (this.liveVideo.videoId == 0) {
+      this.liveVideo.videoId = 0;
+      this.liveVideo.title = arr_selected.title;
+      this.liveVideo.src = arr_selected.youtube_url;
+      //this.liveVideo.matchType=arr_selected.matchType;
+      this.liveVideo.matchId = arr_selected._id;
+      this.liveVideo.url=arr_selected.url;
+
+      if(arr_selected.url!=""){
+        this.liveVideo.showurl=true;
+      }
+      else{
+        this.liveVideo.showurl=false;
+      }
+
+    }
+
+    var i_value = 1;
+    for (var i = i_value; i <= Object.keys(arr_selected.innings_1).length; i++) {
+
+      const vidObject: videoInfo_index = { matchId: 0, matchType: "", videoId: 0, src: "", showOnTop: false, title: "", img: "", index: 0,url:"",showurl:false };
+      vidObject.matchId = arr_selected.innings_1[i-1]._id,
+        vidObject.src = arr_selected.innings_1[i-1].url + '?autoplay=1';
+      vidObject.title = arr_selected.innings_1[i-1].title;
+      vidObject.videoId = i;
+      vidObject.url=arr_selected.innings_1[i-1].url;
+      if(arr_selected.innings_1[i-1].url!=""){
+        vidObject.showurl=true;
+      }
+      else{
+        vidObject.showurl=false;
+      }
+
+      this.inning_1_info.push(vidObject);
+      i_value = i;
+    }
+
+
+   // i_value = i_value + 1;
+    for (var i = i_value; i <=Object.keys(arr_selected.innings_2).length; i++) {
+
+      const vidObject: videoInfo_index = { matchId: 0, matchType: "", videoId: 0, src: "", showOnTop: false, title: "", img: "", index: 0,url:"",showurl:false };
+      vidObject.matchId = arr_selected.innings_1[i-1]._id,
+        vidObject.src = arr_selected.innings_1[i-1].url + '?autoplay=1';
+      vidObject.title = arr_selected.innings_1[i-1].title;
+      vidObject.videoId = i + i_value;
+      i_value = i + i_value;
+      vidObject.url=arr_selected.innings_2[i-1].url;
+
+      if(arr_selected.innings_2[i-1].url!=""){
+        vidObject.showurl=true;
+      }
+      else{
+        vidObject.showurl=false;
+      }
+      this.inning_2_info.push(vidObject);
+    }
+
+
+
+    this.innings_1 = [];
+    this.innings_2 = [];
+    for (var i = 0; i < this.inning_1_info.length; i++) {
+      if (this.inning_1_info[i].videoId == this.videoId) {
+        this.topVideo = this.inning_1_info[i];
+        this.topVideo.src = this.topVideo.src + "?autoplay=1";
+        this.innings_1.push(this.liveVideo)
+      }
+      else {
+        this.innings_1.push(this.inning_1_info[i])
+      }
+
+    }
+    for (var i = 0; i < this.inning_2_info.length; i++) {
+
+      if (this.inning_2_info[i].videoId == this.videoId) {
+        this.topVideo = this.inning_2_info[i];
+        this.topVideo.src = this.topVideo.src + "?autoplay=1";
+        this.innings_2.push(this.liveVideo)
+      }
+      else {
+        this.innings_2.push(this.inning_2_info[i])
+      }
+    }
+
+    if (this.topVideo.videoId === 0) {
+      this.topVideo = this.liveVideo;
+    }
+    console.log(this.innings_1)
+    console.log(this.innings_2)
+
+
+    this.show_innings1 = this.innings_1.length > 0 ? true : false;
+    this.show_innings2 = this.innings_2.length > 0 ? true : false;
+    //this.products = data;
+
+
+    console.log(this.show_innings1)
+  }
+  swapVideos(selectedMatchId: number, selectedVideoId: number) {
+    console.log("comes here now");
   }
 
-  loadVideos(selectedMatchId:number,selectedVideoId:number){
-    console.log(this.match+"matchId true")
-    this.httpClient.get("/assets/js/liveMatches.json").subscribe(data =>{
-      console.log(data +"this info");
-      console.log(data["matchInfo"].length)
-      var arr_selected=data["matchInfo"].find(el=>el.matchId==this.match)
-      console.log("found the array")
-      console.log(arr_selected)
-      if(this.liveVideo.videoId==0)
-      {
-        this.liveVideo.videoId=arr_selected.videoId;
-        this.liveVideo.title=arr_selected.title;
-        this.liveVideo.src=arr_selected.src;
-        this.liveVideo.matchType=arr_selected.matchType;
-        this.liveVideo.matchId=arr_selected.matchId;
-
-      }
-      var inning_1_info=arr_selected.videos.innings_1;
-      var inning_2_info=arr_selected.videos.innings_2;
-      this.innings_1=[];
-      this.innings_2=[];
-      for(var i=0;i<inning_1_info.length;i++){
-        if(inning_1_info[i].videoId==this.videoId)
-        {
-          this.topVideo=inning_1_info[i];
-          this.topVideo.src=this.topVideo.src+"?autoplay=1";
-          this.innings_1.push(this.liveVideo)
-        }
-        else{
-        this.innings_1.push(inning_1_info[i])
-        }
-
-      }
-      for(var i=0;i<inning_2_info.length;i++){
-
-        if(inning_2_info[i].videoId==this.videoId)
-        {
-          this.topVideo=inning_2_info[i];
-          this.topVideo.src=this.topVideo.src+"?autoplay=1";
-          this.innings_2.push(this.liveVideo)
-        }
-        else{
-        this.innings_2.push(inning_2_info[i])
-        }
-      }
-
-      if(this.topVideo.videoId===0){
-        this.topVideo=this.liveVideo;
-      }
-console.log(this.innings_1)
-console.log(this.innings_2)
-      //this.products = data;
-    })
-
-  }
-  swapVideos(selectedMatchId:number,selectedVideoId:number){
-      console.log("comes here now");
-  }
-
-  videoURL(url:string) {
+  videoURL(url: string) {
     return this._sanitizer.bypassSecurityTrustResourceUrl(url);
   }
+
+
 
 
 }
--- src/app/Home/playlist/playlist.component.ts
+++ src/app/Home/playlist/playlist.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient,HttpClientJsonpModule,HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpClientJsonpModule,
+  HttpParams,
+  HttpHeaders,
+} from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NumberSymbol } from '@angular/common';
 import { FormControl, Validators } from '@angular/forms';
 
-
-
-
 export interface videoInfo {
-  videoId:number,
-  src:string,
-  title:string,
-  matchId:number,
-  matchType:string,
-  showOnTop:boolean,
-  img:string
-
+  videoId: number;
+  src: string;
+  title: string;
+  matchId: number;
+  matchType: string;
+  showOnTop: boolean;
+  img: string;
+  url:string;
+  showurl:boolean;
 }
 interface MailChimpResponse {
   result: string;
@@ -26,58 +29,129 @@ interface MailChimpResponse {
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.css']
+  styleUrls: ['./playlist.component.css'],
 })
 export class PlaylistComponent implements OnInit {
+  name = '';
+  topSafeUrl = '';
+  topVideo: videoInfo = {
+    matchId: 0,
+    matchType: '',
+    videoId: 0,
+    src: '',
+    showOnTop: false,
+    title: '',
+    img: '',
+    url:'',
+    showurl:false,
+  };
 
-  name="";
-  topSafeUrl="";
-  topVideo:videoInfo={matchId:0,matchType:"",videoId:0,src:"",showOnTop:false,title:"",img:""};
-  otherVideos:Array<videoInfo>=[];
+  otherVideos: Array<videoInfo> = [];
   submitted = false;
-	//mailChimpEndpoint = 'https://username.us6.list-manage.com/subscribe/post-json?u=abc123&amp;id=123&';
-	error = '';
- 
-  constructor(private httpClient:HttpClient,private _sanitizer:DomSanitizer) { }
+  //mailChimpEndpoint = 'https://username.us6.list-manage.com/subscribe/post-json?u=abc123&amp;id=123&';
+  error = '';
 
-  ngOnInit(): void {
+  constructor(
+    private httpClient: HttpClient,
+    private _sanitizer: DomSanitizer
+  ) {}
 
-    this.httpClient.get("/assets/js/liveMatches.json").subscribe(data =>{
+  async ngOnInit() {
+    /* this.httpClient.get('/assets/js/liveMatches.json').subscribe((data) => {
       console.log(data);
-      console.log(data["matchInfo"].length)
+      console.log(data['matchInfo'].length);
 
-      for(var i=0;i<data["matchInfo"].length;i++){
-        if(data["matchInfo"][i].showOnTop){
-          this.topVideo=data["matchInfo"][i];
-          this.topVideo.src=this.topVideo.src+"?autoplay=1"
+      for (var i = 0; i < data['matchInfo'].length; i++) {
+        if (data['matchInfo'][i].showOnTop) {
+          this.topVideo = data['matchInfo'][i];
+          this.topVideo.src = this.topVideo.src + '?autoplay=1';
+        } else {
+          this.otherVideos.push(data['matchInfo'][i]);
         }
-        else{
-          this.otherVideos.push(data["matchInfo"][i]);
-        }
-
       }
 
-      
-     this.topSafeUrl==this._sanitizer.bypassSecurityTrustUrl(this.topVideo.src)
-      console.log(this.topVideo)
-      console.log(this.otherVideos)
+      this.topSafeUrl ==
+        this._sanitizer.bypassSecurityTrustUrl(this.topVideo.src);
+      console.log(this.topVideo);
+      console.log(this.otherVideos);
       //this.products = data;
-    })
+    }); */
+
+    await this.getVideos();
   }
 
-  videoURL(url:string) {
+  videoURL(url: string) {
     return this._sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
-  emailControl = new FormControl('', [
-		Validators.required,
-		Validators.email,
-	]);
-	nameControl = new FormControl('', [
-		Validators.required
-	]);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  nameControl = new FormControl('', [Validators.required]);
 
-	/* submit() {
+  async getVideos() {
+    var corsHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+    const vidData = await this.httpClient
+      .get(
+        'http://localhost:3000/api/match/getmatch/60cd39a12523c92d20603802',
+        { headers: corsHeaders }
+      )
+      .toPromise();
+
+    for (var i = 0; i < Object.keys(vidData).length; i++) {
+      if (i == 0) {
+          //this.topVideo = data['matchInfo'][i];
+          this.topVideo.matchId=vidData[i]._id;
+          this.topVideo.src=vidData[i].youtube_url+ '?autoplay=1';
+          this.topVideo.title=vidData[i].title;
+          this.topVideo.videoId=i;
+          this.topVideo.url=vidData[i].url;
+          if(vidData[i].url!=""){
+            this.topVideo.showurl=true;
+          }
+          else{
+
+            this.topVideo.showurl=false;
+          }
+          
+          //this.topVideo.src = this.topVideo.src + '?autoplay=1';
+          this.topSafeUrl ==
+          this._sanitizer.bypassSecurityTrustUrl(this.topVideo.src);
+        
+      }else {
+        //this.otherVideos.push(data['matchInfo'][i]);
+        const vidObject:videoInfo = {
+          matchId: 0,
+          matchType: '',
+          videoId: 0,
+          src: '',
+          showOnTop: false,
+          title: '',
+          img: '',
+          url:'',
+          showurl:false
+        };
+        vidObject.matchId=vidData[i]._id,
+        vidObject.src=vidData[i].youtube_url+ '?autoplay=1';
+        vidObject.title=vidData[i].title;
+        vidObject.videoId=i;
+        vidObject.url=vidData[i].url;
+        if(vidObject.url!=""){
+          vidObject.showurl=true;
+        }
+        else{
+
+          vidObject.showurl=false;
+        }
+        this.otherVideos.push(vidObject);
+
+      }
+    }
+    console.log(this.topVideo);
+    console.log(this.otherVideos);
+  }
+  /* submit() {
 		this.error = '';
 		if (this.emailControl.status === 'VALID' && this.nameControl.status === 'VALID') {
 
@@ -102,6 +176,4 @@ export class PlaylistComponent implements OnInit {
 			});
 		}
 	} */
-
-
 }
